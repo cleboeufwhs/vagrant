e56ae9dbb4900576a90c54ee16e2683596ec0862
Cole LeBoeuf <cleboeufwhs@gmail.com>
2019-08-16 14:00:46 +0000

    created a clear in .scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//=require jquery
 //= require turbolinks
+//= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
